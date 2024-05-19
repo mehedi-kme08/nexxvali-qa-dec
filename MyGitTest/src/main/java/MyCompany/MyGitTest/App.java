c4562bd88b43c0c24bfc45008d394cbfc5d5333e
mehedime91 <mhasan.kme@gmail.com>
2024-05-19 00:51:06 -0400

    MyGitTest project is ready to push

---
+++ MyGitTest/src/main/java/MyCompany/MyGitTest/App.java
@@ -0,0 +1,13 @@
+package MyCompany.MyGitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
